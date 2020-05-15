6faf5a3511da3f8876671be929d2336b666b00a4
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-05-15 23:55:57 +0900

    [ADD] default.js

---
+++ vanilla.js_Foundation.js/default.js
@@ -0,0 +1,39 @@
+// 버튼 추가
+function addBtn(){
+    const li = document.createElement("li")
+  const delBtn = document.createElement("button")
+  delBtn.innerText = "X"
+  delBtn.addEventListener("click",deleteToDo)
+
+  const span  = document.createElement("span")
+  const newId = toDos.length +1;
+  span.innerText=text
+  li.append(span);
+  li.append(delBtn);
+  li.id = newId
+
+  toDoList.appendChild(li)
+
+}
+
+
+//인풋 태그 만들기
+const toDoForm = document.querySelector('.js-toDoForm')
+toDoForm.addEventListener("submit",handleSubmit)
+
+function handleSubmit(event) {
+    toDoInput = toDoForm.querySelector('input'),
+    event.preventDefault()
+    const currentValue = toDoInput.value
+    paintToDo(currentValue)
+    toDoInput.value=""
+}
+
+// 로컬 스커리지에 저장
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos))
+  }
+
+// 로컬 스터리지에 가져오기
+const parsedToDos = JSON.parse(loadedToDos)
