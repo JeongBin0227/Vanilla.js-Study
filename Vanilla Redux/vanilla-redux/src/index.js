d22c64bdeadecfb0a56a94a05c90a56583146f22
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-09-01 18:07:00 +0900

    [ADD] Subscriptions set

--- Vanilla Redux/vanilla-redux/src/index.js
+++ Vanilla Redux/vanilla-redux/src/index.js
@@ -8,13 +8,27 @@ const countModifier = (count = 0,action) => {
     if(action.type==="ADD"){
         return count+1;
     }
+    else if(action.type==="MINUS"){
+        return count-1;
+    }
     return count
 }
 
+const onChange = () => {
+    // console.log('there was a change on the store')
+    numberPlace.innerHTML = countStore.getState()
+    // console.log()
+}
+
+
 const countStore = createStore(countModifier)
 
+countStore.subscribe(onChange)
 
-countStore.dispatch({type:'ADD'})
+
+plusButton.addEventListener("click",()=>countStore.dispatch({type:"ADD"}))
+minusButton.addEventListener("click",()=>countStore.dispatch({type:"MINUS"}))
+
 console.log(countStore.getState())
 
 // let number = 0
