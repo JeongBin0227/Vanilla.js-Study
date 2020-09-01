0a2aa7c7fc734c0aa8b4efc83f5ed12fd2bfeea0
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-09-01 16:50:41 +0900

    [ADD] Store Setting

--- Vanilla Redux/vanilla-redux/src/index.js
+++ Vanilla Redux/vanilla-redux/src/index.js
@@ -1,27 +1,36 @@
+import {createStore} from 'redux'
+
 const plusButton =document.getElementById('add');
 const minusButton =document.getElementById('minus');
 const numberPlace =document.querySelector('h1');
 
-let number = 0
-
-
-const updateText = () => {
-    numberPlace.innerHTML = number
+const countModifier = (count = 0) => {
+    return count
 }
 
-updateText()
+const countStore = createStore(countModifier)
+
+console.log(countStore.getState())
+
+// let number = 0
 
 
-const handleAdd = () => {
-    number += 1
-    updateText()
-}
-const handleMinus = () => {
-    number -= 1
-    updateText()
-}
+// const updateText = () => {
+//     numberPlace.innerHTML = number
+// }
+
+// updateText()
 
 
-plusButton.addEventListener('click',handleAdd)
+// const handleAdd = () => {
+//     number += 1
+//     updateText()
+// }
+// const handleMinus = () => {
+//     number -= 1
+//     updateText()
+// }
 
-minusButton.addEventListener('click',handleMinus)
+
+// plusButton.addEventListener('click',handleAdd)
+// minusButton.addEventListener('click',handleMinus)
