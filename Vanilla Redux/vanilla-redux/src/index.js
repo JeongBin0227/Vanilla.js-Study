c2e02ca0a0ca7895cc21fda819880025b5f3431d
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-09-01 18:00:05 +0900

    [ADD] Action set

--- Vanilla Redux/vanilla-redux/src/index.js
+++ Vanilla Redux/vanilla-redux/src/index.js
@@ -4,12 +4,17 @@ const plusButton =document.getElementById('add');
 const minusButton =document.getElementById('minus');
 const numberPlace =document.querySelector('h1');
 
-const countModifier = (count = 0) => {
+const countModifier = (count = 0,action) => {
+    if(action.type==="ADD"){
+        return count+1;
+    }
     return count
 }
 
 const countStore = createStore(countModifier)
 
+
+countStore.dispatch({type:'ADD'})
 console.log(countStore.getState())
 
 // let number = 0
