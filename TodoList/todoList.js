672132430a84cf2f9b29ca94ed90c15acec7e58e
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-09-21 23:54:34 +0900

    [Refactor]

--- TodoList/todoList.js
+++ TodoList/todoList.js
@@ -51,17 +51,12 @@ let todos = {
 
 
 const reducer = (action) => {
-    console.log(action[0])
-    console.log(action[1])
     let tmp = {}
     switch (action[0]) {
         case 'ADD_TODO':
-          console.log(currentTodo)
           for(loopdate in todos){
             if(loopdate===currentTodo) tmp = todos[loopdate]
           }
-          // let tmp = todos.filter(todo=>todo[`${currentTodo}`]!=undefined)
-          console.log(tmp)
           if(Object.keys(tmp).length===0) {
             todos[currentTodo] = {}
             todos[currentTodo][`${action[1]}` ] =  {}
@@ -77,38 +72,20 @@ const reducer = (action) => {
           paintToDos()
           break;
         case 'DELETE_TODO':
-          console.log('DELETE_TODO')
-
-          // for(loopdate in todos){
-          //   if(loopdate===currentTodo) tmp = todos[loopdate]
-          // }
-          // let tmp = todos.filter(todo=>todo[`${currentTodo}`]!=undefined)
-
-          console.log( todos[`${currentTodo}`])
-
           for(key in todos){
-            let tmp = {}
-            tmp[key]={}
+            let tmpTodo = {}
+            tmpTodo[key]={}
             for(todo in todos[key]){
               if((todos[key][todo]['id']!==Number(action[1]))){
-                tmp[key][todo]={}
-                tmp[key][todo] = todos[key][todo]
+                tmpTodo[key][todo]={}
+                tmpTodo[key][todo] = todos[key][todo]
               }
             }
-            todos[key]=tmp[key]
+            todos[key]=tmpTodo[key]
           }
-          // console.log( todos[`${currentTodo}`])
-          // for( todo in todos[currentTodo]){
-          //   console.log(todos[currentTodo][todo]['id'])
-          // }
-          // todos[`${currentTodo}`] = todos[`${currentTodo}`].filter(todo=>todo.id)
-          console.log( todos[`${currentTodo}`])
           paintToDos()
-
-          // return state.filter(todo=>todo.id!=action.id)
           break;
         default:
-          // return state
           break;
     }
 }
@@ -125,8 +102,6 @@ function paintToDos(){
 
 //todo 생성
 const createToDo = (todo,id) => {
-  console.log(todo)
-  console.log(id)
   const li = document.createElement('li')
   li.id = id
   li.innerHTML=todo
@@ -146,8 +121,6 @@ const createToDo = (todo,id) => {
 //투두리스트 추가 눌렀을 때
 const onSubmit = e => {
   e.preventDefault()
-  // dispatchAddToDo(todoInput.value)
-  // console.log(todoInput)
   let currentId = ''
   currentId = year.innerHTML+(changeMonth+1)+selectWeek.innerHTML
 
