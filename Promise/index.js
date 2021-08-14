736f07145d683640d035d4a5808755efdfad2e5c
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-08-14 23:54:23 +0900

    feat: study 자료 Promise resolve reject

--- Promise/index.js
+++ Promise/index.js
@@ -1,1 +1,13 @@
 // console.log(Promise);
+
+// const p = () => {
+//   return new Promise((resolve, reject) => {
+//     // pending
+//     setTimeout(() => {
+//       resolve("then"); /* fullfilled */
+//     }, 1000);
+
+//     // setTimeout(() => {
+//     //   reject(); /* fullfilled */
+//     // }, 1000);
+//   });
