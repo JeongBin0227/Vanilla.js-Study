4909c286a51a44bf1fe8c382b109b8cd69be0cf9
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-08-14 23:54:34 +0900

    feat: study 자료 Promise callback

--- Promise/index.js
+++ Promise/index.js
@@ -11,3 +11,36 @@
 //     //   reject(); /* fullfilled */
 //     // }, 1000);
 //   });
+// };
+
+// //callback 작성 공간
+// p()
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch(() => {
+//     console.log("b");
+//   })
+//   .finally(() => {
+//     console.log("ss");
+//   });
+
+// function c(callback) {
+//   setTimeout(() => {
+//     callback();
+//   }, 1000);
+// }
+
+// c(() => {
+//   console.log("1000ms 후에 callback 함수가 실행됩니다.");
+// });
+
+// c(() => {
+//   c(() => {
+//     c(() => {
+//       //3초후에
+//       console.log("1000ms 후에 callback 함수가 실행됩니다.");
+//     });
+//   });
+// });
+
